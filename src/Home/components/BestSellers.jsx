124f9245c2b4e29e7e192bd3e5ecfb279793ec5a
fathima-nihala <137775396+fathima-nihala@users.noreply.github.com>
2024-09-03 16:01:06 +0530

    init

--- src/Home/components/BestSellers.jsx
+++ src/Home/components/BestSellers.jsx
@@ -16,7 +16,7 @@ import bst8 from '../../assets/slider/bstseller8.jpg';
 const BestSellers = () => {
     return (
         <div className="px-4 py-8">
-            <div className="text-center text-4xl font-semibold mb-2">Our Best Sellers...</div>
+            <div className="text-center text-4xl font-semibold ">Our Best Sellers...</div>
             <div className="text-center text-2xl font-medium text-opacity-70 mb-8">High-Precision Equipment Trusted by Experts</div>
             <Swiper
                 modules={[Autoplay, Pagination, Navigation]}
@@ -26,77 +26,91 @@ const BestSellers = () => {
                 navigation
                 autoplay={{ delay: 3000, disableOnInteraction: false }}
                 className="swiper-container"
+                breakpoints={{
+                    320:{
+                        slidesPerView: 1,
+                    },
+                    375: {
+                        slidesPerView: 2, // 2 slides per view on mobile devices
+                    },
+                    768: {
+                        slidesPerView: 3, // 3 slides per view on tablets
+                    },
+                    1024: {
+                        slidesPerView: 3.5, // 3.5 slides per view on larger screens
+                    },
+                }}
             >
-                <SwiperSlide className="swiper-slide">
-                    <div className="border border-gray-300 rounded-xl p-4 ">
-                        <div className="flex justify-center items-center mb-4">
+                <SwiperSlide className="swiper-slide"  style={{ paddingBottom: '40px' }}>
+                    <div className="border border-gray-300 rounded-xl p-6 ">
+                        <div className="flex justify-center items-center ">
                             <img src={bst1} alt="Best Seller 1" className="w-full h-full object-cover rounded-lg" />
                         </div>
-                        <p className="text-lg font-semibold mb-2">nice 2000 B _ niceneotech</p>
+                        <p className="text-lg font-semibold ">nice 2000 B </p>
                         <p className="text-xl">$14.99</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="swiper-slide">
-                    <div className="border border-gray-300 rounded-xl p-4 ">
-                        <div className="flex justify-center items-center mb-4">
+                    <div className="border border-gray-300 rounded-xl p-6 ">
+                        <div className="flex justify-center items-center ">
                             <img src={bst2} alt="Best Seller 2" className="w-full h-full object-cover rounded-lg" />
                         </div>
-                        <p className="text-lg font-semibold mb-2">Edan iM3</p>
+                        <p className="text-lg font-semibold ">Edan iM3</p>
                         <p className="text-xl">$19.99</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="swiper-slide">
-                    <div className="border border-gray-300 rounded-xl p-4 ">
-                        <div className="flex justify-center items-center mb-4">
+                    <div className="border border-gray-300 rounded-xl p-6 ">
+                        <div className="flex justify-center items-center ">
                             <img src={bst3} alt="Best Seller 2" className="w-full h-full object-cover rounded-lg" />
                         </div>
-                        <p className="text-lg font-semibold mb-2">SonoScape E2</p>
+                        <p className="text-lg font-semibold ">SonoScape E2</p>
                         <p className="text-xl"> $18.99</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="swiper-slide">
-                    <div className="border border-gray-300 rounded-xl p-4 ">
-                        <div className="flex justify-center items-center mb-4">
+                    <div className="border border-gray-300 rounded-xl p-6 ">
+                        <div className="flex justify-center items-center ">
                             <img src={bst4} alt="Best Seller 2" className="w-full h-full object-cover rounded-lg" />
                         </div>
-                        <p className="text-lg font-semibold mb-2">F3 Edan Medical Fetal Monitor</p>
+                        <p className="text-lg font-semibold ">F3 Edan</p>
                         <p className="text-xl">$20.99</p>
                     </div>
                 </SwiperSlide>
                
                   <SwiperSlide className="swiper-slide">
-                    <div className="border border-gray-300 rounded-xl p-4 ">
-                        <div className="flex justify-center items-center mb-4">
+                    <div className="border border-gray-300 rounded-xl p-6 ">
+                        <div className="flex justify-center items-center ">
                             <img src={bst5} alt="Best Seller 2" className="w-full h-full object-cover rounded-lg" />
                         </div>
-                        <p className="text-lg font-semibold mb-2">Edan iM50 </p>
-                        <p className="text-xl">  $14.99</p>
+                        <p className="text-lg font-semibold ">Edan iM50 </p>
+                        <p className="text-xl">$14.99</p>
                     </div>
                 </SwiperSlide>
                   <SwiperSlide className="swiper-slide">
-                    <div className="border border-gray-300 rounded-xl p-4 ">
-                        <div className="flex justify-center items-center mb-4">
+                    <div className="border border-gray-300 rounded-xl p-6 ">
+                        <div className="flex justify-center items-center ">
                             <img src={bst6} alt="Best Seller 2" className="w-full h-full object-cover rounded-lg" />
                         </div>
-                        <p className="text-lg font-semibold mb-2">iM20 Modular</p>
+                        <p className="text-lg font-semibold ">iM20 Modular</p>
                         <p className="text-xl"> $14.99</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="swiper-slide">
-                    <div className="border border-gray-300 rounded-xl p-4 ">
-                        <div className="flex justify-center items-center mb-4">
+                    <div className="border border-gray-300 rounded-xl p-6 ">
+                        <div className="flex justify-center items-center ">
                             <img src={bst7} alt="Best Seller 2" className="w-full h-full object-cover rounded-lg" />
                         </div>
-                        <p className="text-lg font-semibold mb-2">ECG</p>
+                        <p className="text-lg font-semibold ">ECG</p>
                         <p className="text-xl"> $14.99</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="swiper-slide">
-                    <div className="border border-gray-300 rounded-xl p-4 ">
-                        <div className="flex justify-center items-center mb-4">
+                    <div className="border border-gray-300 rounded-xl p-6 ">
+                        <div className="flex justify-center items-center ">
                             <img src={bst8} alt="Best Seller 2" className="w-full h-full object-cover rounded-lg" />
                         </div>
-                        <p className="text-lg font-semibold mb-2">G2S BPAP System</p>
+                        <p className="text-lg font-semibold ">G2S BPAP System</p>
                         <p className="text-xl">$24.99</p>
                     </div>
                 </SwiperSlide>
