33fa10f06157ccaba8e9ccbb9ccc6321a9ad2cfe
Nihala <fathimanihala200214@gmail.com>
2024-09-04 00:00:29 +0530

    init

--- src/Home/components/Blogs.jsx
+++ src/Home/components/Blogs.jsx
@@ -30,7 +30,7 @@ const Blogs = () => {
 
     return (
         <div className="container mx-auto">
-            <div className="px-12">
+            <div className="px-12 py-11">
                 <h2 className="text-3xl font-bold">From Our Blog</h2>
                 <p className="text-gray-600">Read our latest articles on real estate.</p>
                 <div className="flex flex-wrap gap-6 mt-4 md:mt-5">
@@ -47,7 +47,7 @@ const Blogs = () => {
                                     <div className="text-gray-800 opacity-50 text-xs uppercase font-bold mb-[5px] transition-all duration-400 ease-in-out group-hover:opacity-0 group-hover:-translate-y-full">
                                         {post.category}
                                     </div>
-                                    <div className="text-gray-800 font-extrabold leading-5 text-lg transition-all duration-400 ease-in-out group-hover:-translate-y-[22px]">
+                                    <div className="text-gray-800 font-extrabold leading-5 lg:text-lg text-[14px] transition-all duration-400 ease-in-out group-hover:-translate-y-[22px]">
                                         {post.title}
                                     </div>
                                     <div className="text-gray-800 opacity-70 text-sm mt-2 transition-all duration-400 ease-in-out group-hover:opacity-0">
@@ -61,7 +61,13 @@ const Blogs = () => {
                         </div>
                     ))}
                 </div>
-                <a href="blog.html" className="text-uppercase mt-2 md:mt-4 pxp-animate">Read More</a>
+                {/* <a href="blog.html" className="text-uppercase mt-2 md:mt-4 pxp-animate">Read More</a> */}
+                <div className="flex items-center mt-4">
+                        <a href="#" className="text-black font-bold uppercase tracking-wider group flex items-center">
+                        Read More
+                            <span className="ml-2 inline-block h-0.5 w-10 bg-black transition-all duration-300 ease-in-out group-hover:w-20"></span>
+                        </a>
+                    </div>
             </div>
         </div>
     );
