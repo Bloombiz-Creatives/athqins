89bd6a4ef1a1b75a542d1e43ac9c637bc087d023
fathima-nihala <137775396+fathima-nihala@users.noreply.github.com>
2024-11-04 17:08:29 +0530

    our client

--- src/Home/components/OurClients.jsx
+++ src/Home/components/OurClients.jsx
@@ -1,54 +1,64 @@
+import img1 from '../../assets/clients/ALMAS.png';
+import img2 from '../../assets/clients/ASTER.png';
+import img3 from '../../assets/clients/BANGLORE HSPTL.png';
+import img4 from '../../assets/clients/BELIEVERS CHURCH.png';
+import img5 from '../../assets/clients/BMH.png';
+import img6 from '../../assets/clients/IQRA.png';
+import img7 from '../../assets/clients/KMCT.png';
+// import img8 from '../../assets/clients/LAKSHYA.png';
+import img9 from '../../assets/clients/MAULANA.png';
+import img10 from '../../assets/clients/MES.png';
+import img11 from '../../assets/clients/MOTHER HOOD.png';
+// import img12 from '../../assets/clients/MOTHER HOSPITAL.png';
+import img13 from '../../assets/clients/SIMSRC.png';
+import img14 from '../../assets/clients/SUT PATTOM.png';
+
+
 const Images = [
     {
-        logoImg: 'img'
+        logoImg: img1,
     },
     {
-        logoImg: 'img'
+        logoImg: img2,
     },
     {
-        logoImg: 'img'
+        logoImg: img3,
     },
     {
-        logoImg: 'img'
+        logoImg: img4,
     },
     {
-        logoImg: 'img'
+        logoImg: img5,
     },
     {
-        logoImg: 'img'
+        logoImg: img6,
     },
     {
-        logoImg: 'img'
+        logoImg: img7,
     },
     {
-        logoImg: 'img'
+        logoImg: img13,
     },
     {
-        logoImg: 'img'
+        logoImg: img9,
     },
     {
-        logoImg: 'img'
+        logoImg: img10,
     },
     {
-        logoImg: 'img'
+        logoImg: img11,
     },
     {
-        logoImg: 'img'
+        logoImg: img14,
     },
-    {
-        logoImg: 'img'
-    },
-    {
-        logoImg: 'img'
-    },
-    {
-        logoImg: 'img'
-    },
-    {
-        logoImg: 'img'
-    },
-]
-
+    // {
+    //     logoImg: img8,
+    // },
+    // {
+    //     logoImg: img14,
+    // },
+   
+];
 
 const OurClients = () => {
 
@@ -59,13 +69,13 @@ const OurClients = () => {
                     <div className='bg-blue-900 h-10 w-1'></div>
                     <h2 className='uppercase text-[30px]  w-full  font-semibold font-sans '>OUR CLIENTS</h2>
                 </div>
-                <div className="mt-10 grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-4">
+                <div className="mt-10 grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-4 mb-12">
                     {Images.map((image, index) => (
                         <div key={index} className="flex items-center justify-center ">
                             <img
                                 src={image.logoImg}
                                 alt={`Client Logo ${index}`}
-                                className="h-20 w-auto object-contain"
+                                className="md:h-[80px] md:w-[160px] h-[50px] w-[100px] object-contain"
                             />
                         </div>
                     ))}
